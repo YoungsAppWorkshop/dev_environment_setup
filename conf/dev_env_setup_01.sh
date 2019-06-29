fb61596252e0c17146b28722086d481a06c06435
YongJe Jung <ryviusm6@gmail.com>
2019-06-29 19:03:04 +0900

    📦 NEW: Dev Environment Setup shell script

---
+++ conf/dev_env_setup_01.sh
@@ -0,0 +1,5 @@
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+echo 'Restart the Terminal and Run brew doctor'
